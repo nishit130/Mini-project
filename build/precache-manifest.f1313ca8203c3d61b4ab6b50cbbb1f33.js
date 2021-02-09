887929f1a2635c0aa98891be9873dff5f2c99c09
nis130 <nishitlimbani130@gmail.com>
2021-02-09 09:19:09 +0530

    Add the frontend and boilerplate
    Created the basic layout of dashboard and created user profile
    page all the data is  currently dummy data and there are some
    other pages that are required to be created

---
+++ build/precache-manifest.f1313ca8203c3d61b4ab6b50cbbb1f33.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "937e2f39f09e40e56152bb36c4fbf56e",
+    "url": "/material-dashboard-react/index.html"
+  },
+  {
+    "revision": "f8c51bd5d57caad1320b",
+    "url": "/material-dashboard-react/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "d21bd03696a9084ac0d2",
+    "url": "/material-dashboard-react/static/css/main.8e9f8467.chunk.css"
+  },
+  {
+    "revision": "f8c51bd5d57caad1320b",
+    "url": "/material-dashboard-react/static/js/2.343371cf.chunk.js"
+  },
+  {
+    "revision": "34491c6de1fbcf2388878b87785c74e7",
+    "url": "/material-dashboard-react/static/js/2.343371cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d21bd03696a9084ac0d2",
+    "url": "/material-dashboard-react/static/js/main.2a29b64f.chunk.js"
+  },
+  {
+    "revision": "f7bdf0cfc9ea0dd44285692948c2d84f",
+    "url": "/material-dashboard-react/static/js/main.2a29b64f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a025ab54ec03fed337bb",
+    "url": "/material-dashboard-react/static/js/runtime-main.d0bf2bb1.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/material-dashboard-react/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/material-dashboard-react/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/material-dashboard-react/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/material-dashboard-react/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/material-dashboard-react/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
---
+++ src/routes.js
@@ -0,0 +1,43 @@
+/*!
+
+=========================================================
+* Material Dashboard React - v1.9.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/material-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+// @material-ui/icons
+import Dashboard from "@material-ui/icons/Dashboard";
+import Person from "@material-ui/icons/Person";
+
+// core components/views for Admin layout
+import DashboardPage from "views/Dashboard/Dashboard.js";
+import UserProfile from "views/UserProfile/UserProfile.js";
+
+const dashboardRoutes = [
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    icon: Dashboard,
+    component: DashboardPage,
+    layout: "/admin"
+  },
+  {
+    path: "/user",
+    name: "User Profile",
+    icon: Person,
+    component: UserProfile,
+    layout: "/admin"
+  },
+];
+
+export default dashboardRoutes;
